75640c1da3479f19784453232b3a494bfc047cb3
Jake Priddy <priddyjacob84@gmail.com>
2017-05-20 14:59:27 -0400

    initial commit

---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour {
+
+	public GameObject orbiter; //the object that will orbit around 
+	public float speed; //speed of ortbit
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		OrbitAround (); /*make the object 1 rotate around object 2 */
+	}
+
+	void OrbitAround() 
+	{
+		//make object 1 orbit around m
+		transform.RotateAround (orbiter.transform.position, Vector3.up, speed * Time.deltaTime); 
+
+	}
+}
